e853652d421114ceacf9303d2b6f289c9ce509be
GSunwoo <ace9sun@naver.com>
2025-05-29 23:08:27 +0900

    delete prev

--- reactapp-project-GSW/src/components/groupchat/GroupChat.jsx
+++
@@ -1,6 +0,0 @@
-function GroupChat(props) {
-  return (<>
-    <h2>대충 그룹채팅</h2>
-  </>); 
-}
-export default GroupChat;
--- reactapp-project-GSW/src/components/searchgroup/Searchgroup.jsx
+++
@@ -1,8 +0,0 @@
-function Searchgroup(props) {
-  return (<>
-    <div className="wrapper">
-      <h2>여기는 그룹찾기</h2>
-    </div>
-  </>); 
-}
-export default Searchgroup;
