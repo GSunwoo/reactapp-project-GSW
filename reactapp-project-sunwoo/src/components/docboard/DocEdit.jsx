b5bae936092022f8840c3bb21d6950a5d94c6a45
GSunwoo <ace9sun@naver.com>
2025-06-03 02:28:11 +0900

    필요없는 부분 삭제

--- reactapp-project-sunwoo/src/components/docboard/DocEdit.jsx
+++ reactapp-project-sunwoo/src/components/docboard/DocEdit.jsx
@@ -6,22 +6,6 @@ import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-function nowDate() {
-  const now = new Date();
-
-  const pad = (num) => String(num).padStart(2, '0');
-
-  const year = now.getFullYear();
-  const month = pad(now.getMonth() + 1); // 0-based
-  const day = pad(now.getDate());
-
-  const hours = pad(now.getHours());
-  const minutes = pad(now.getMinutes());
-  const seconds = pad(now.getSeconds());
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
 function DocEdit(props) {
   const { itsMe } = useAuth();
   const { docs } = useDoc();
@@ -35,7 +19,6 @@ function DocEdit(props) {
   const navigate = useNavigate();
 
   const boardPath = '/doc-board/';
-
 
   const getNowDoc = () => {
     if (!docs) return;
